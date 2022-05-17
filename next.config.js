0229ba4a97513707e3c1bda51aef774328664484
Ernest Cui <ecuiships@126.com>
2022-05-17 22:06:59 +0800

    go to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath:"/Automata-Playground"
+  basePath: "/Automata-Playground",
+  assetPrefix: "/Automata-Playground"
 }
 
 module.exports = nextConfig
